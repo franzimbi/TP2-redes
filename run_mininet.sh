c27dd145ba99fe83bc99cf9ae9879644b0830a05
franzimbi <fzimbimbakis@fi.uba.ar>
2025-06-25 20:10:49 -0300

    agrego run_mininet y agrego l2 learning a pox

---
+++ run_mininet.sh
@@ -0,0 +1,5 @@
+TOPO_SIZE="3"
+
+sudo mn -c
+
+sudo python3 topologia.py $TOPO_SIZE
--- run_pox.sh
+++ run_pox.sh
@@ -3,4 +3,4 @@ POX_MODULE="ext.firewall"
 
 POX_ARGS="--verbose"
 
-./pox/pox.py samples.pretty_log $POX_ARGS $POX_MODULE
+./pox/pox.py samples.pretty_log  $POX_ARGS forwarding.l2_learning $POX_MODULE
